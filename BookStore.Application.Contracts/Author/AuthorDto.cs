ffce27833bf2f4975cd301597b8313538b5d64cd
alxmcs <70561974+alxmcs@users.noreply.github.com>
2025-03-03 15:09:36 +0400

    bugfix

--- BookStore.Application.Contracts/Author/AuthorDto.cs
+++ BookStore.Application.Contracts/Author/AuthorDto.cs
@@ -7,5 +7,5 @@
 /// <param name="FirstName">Имя</param>
 /// <param name="Patronymic">Отчество</param>
 /// <param name="Biography">Биография</param>
-/// <param name="WorkCount">Число работ</param>
+/// <param name="BookCount">Число работ</param>
 public record AuthorDto(int Id, string? LastName, string? FirstName, string? Patronymic, string? Biography, int? BookCount);
